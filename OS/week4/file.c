519cbea9104a8c828c75d4ee533e97405484d182
Gatha Sreesan <gathasreesan@gmail.com>
2026-01-02 11:03:57 +0530

    Create file.c

---
+++ OS/week4/file.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+    int src = open("input.txt", O_RDONLY);
+    int dest = open("output.txt", O_WRONLY | O_CREAT, 0644);
+    char buffer[100];
+    int n;
+
+    while ((n = read(src, buffer, sizeof(buffer))) > 0) {
+        write(dest, buffer, n);
+    }
+
+    close(src);
+    close(dest);
+
+    return 0;
+}
