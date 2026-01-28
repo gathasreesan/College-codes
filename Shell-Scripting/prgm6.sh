ac9fd406ca9aa6c1bf9c44068cd60e837d029074
Gatha Sreesan <gathasreesan@gmail.com>
2026-01-28 20:39:38 +0530

    Update prgm6.sh

--- Shell-Scripting/prgm6.sh
+++ Shell-Scripting/prgm6.sh
@@ -1,14 +1,15 @@
 echo "Enter a number"
 read n
 fact=1
-if [n -eq 0]
+if [ $n -eq 0 ]
 then 
   echo "Factorial = 1"
-elif [n -eq 1]
+elif [ $n -eq 1 ]
 then 
   echo "Factorial =1"
 fi
-for i in {2 .. n}
+for ((i=2; i<=n; i++))
 do 
-  fact= fact*i
+  fact=$((fact*i))
 done 
+echo "Factorial = $fact"
