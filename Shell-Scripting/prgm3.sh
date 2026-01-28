57a806226d05c40c510f9fd33775f584455e774d
Gatha Sreesan <gathasreesan@gmail.com>
2026-01-28 19:43:04 +0530

    Add arithmetic operations to prgm3.sh

---
+++ Shell-Scripting/prgm3.sh
@@ -0,0 +1,14 @@
+echo "Enter first number"
+read a
+echo "Enter second number"
+read b
+sum=$((a+b))
+echo "Sum=$sum"
+sub=$((a-b))
+pdt=$((a*b))
+div=$((a/b))
+echo "Difference= $sub"
+echo "Product=$pdt"
+echo "Division=$div"
+
+
