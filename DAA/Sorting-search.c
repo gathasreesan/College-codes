b45efb4d8ebc421e1528539357ee397718367fa9
Gatha Sreesan <gathasreesan@gmail.com>
2026-02-17 13:44:06 +0000

    update code

--- DAA/Sorting-search.c
+++ DAA/Sorting-search.c
@@ -1,20 +1,6 @@
-
-
-1 of 1,023
-(no subject)
-Inbox
-
-GATHA SREESAN RSET <u2409029@rajagiri.edu.in>
-Attachments
-4:18 PM (2 hours ago)
-to me
-
-
- 8 Attachments
-  •  Scanned by Gmail
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 void linear(int arr[], int n, int key)
 {
     int found = 0;
@@ -22,7 +8,7 @@ void linear(int arr[], int n, int key)
     {
         if(arr[i] == key)
         {
-            printf("Element found at index %d\n", i);
+            printf("Linear Search :Element found at index %d\n", i);
             found = 1;
             break;
         }
@@ -33,21 +19,16 @@ void linear(int arr[], int n, int key)
 int binary(int arr[], int n, int key)
 {
     int low = 0, high = n - 1, mid;
-
     while (low <= high)
     {
         mid = (low + high) / 2;
-
         if (arr[mid] == key)
             return mid;
-
         else if (key < arr[mid])
             high = mid - 1;
-
         else
             low = mid + 1;
     }
-
     return -1; // only return -1 AFTER loop ends
 }
 void swap(int *a,int *b)
@@ -65,38 +46,63 @@ for(int j=low;j<high;j++)
 if(A[j]<pivot)
 {
 i++;
-swap(&A[i],&A[j])
+swap(&A[i],&A[j]);
 }
-void quicksort(int arr[],int low,int high)
+}
+return i+1;
+}
+void quicksort(int A[],int low,int high)
 {
 if(low<high)
 {
 int pi=partition(A,low,high);
-quickSort(A,low,pi-1);
-quickSort(A,pi+1,high);
+quicksort(A,low,pi-1);
+quicksort(A,pi+1,high);
 }
 }
-
-
-
 int main()
 {
-int arr[100],arr1[100],n,key,b;
+int n,key,b;
+printf("\n");
 printf("Enter the size of array");
 scanf("%d",&n);
-printf("Enter the elements of the array");
-for(int i=0;i<n;i++)
-{
-scanf("%d",&arr[i]);
-}
-printf("Enter the element to search using linear search");
+int arr[n],arr1[n];
+ srand(time(NULL)); // seeds rand() so numbers change every run
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 1000;   // random number between 0 and 999
+    }
+    // Print them:
+    printf("Random numbers: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d \t", arr[i]);
+    }
+printf("\nEnter the element to search ");
 scanf("%d",&key);
+for (int i = 0; i < n; i++)
+{
+ arr1[i] = arr[i];
+} 
+clock_t start, end;
+double time_taken;
+//Linear Search Time
+printf("\n");
+printf("-----Performance Analysis-----\n");
+printf("Array Size: %d\n", n);
+printf("\n");
+printf(" ----Linear Search:----\n"  );
+start = clock();
 linear(arr,n,key);
-arr1=arr;
-// sort before binary search
+end=clock();
+time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+printf("Time taken by Linear Search: %f seconds\n", time_taken);
+//Bubble Sort Time
+printf("\n");
+printf(" ----Bubble Sort:----\n"  );
+start=clock();
+//bubble sort
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+            if (arr1[j] > arr1[j + 1]) {
                 int temp = arr1[j];
                 arr1[j] = arr1[j + 1];
                 arr1[j + 1] = temp;
@@ -104,14 +110,28 @@ arr1=arr;
         }
     }
 
-    b = binary(arr1, n, key);
-
+end=clock();
+time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+printf("Time taken by Bubble Sort: %f seconds\n", time_taken);
+//Binary Search Time
+printf("\n");
+printf(" ----Binary Search:----\n"  );
+start = clock();
+ b = binary(arr1, n, key);
     if(b != -1)
         printf("Binary Search: Element found at index %d\n", b);
     else
         printf("Binary Search: Element not found\n");
-
-
+end=clock();
+time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+printf("Time taken by Binary Search: %f seconds\n", time_taken);
+//Quick Sort Time
+printf("\n");
+printf(" ----Quick Sort:----\n"  );
+start = clock();
+quicksort(arr, 0, n - 1);
+end=clock();
+time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+printf("Time taken by Quick Sort: %f seconds\n", time_taken);   
 return 0;
 }
-
